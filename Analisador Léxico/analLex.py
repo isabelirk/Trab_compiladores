6e434dab43853b9306c660a9e7e7749ed1821759
CleissonVieira <cleisson.raimundi@gmail.com>
2019-11-19 13:53:01 -0300

    init

---
+++ Analisador Léxico/analLex.py
@@ -0,0 +1,13 @@
+# codigoFonte = open('codigo.txt', 'r')
+# ts = open('ts.txt', 'a')
+
+# for linha in codigoFonte: # Percorre todas as linhas  
+#     for caracter in linha: # Percore todos os caracteres da linha
+#         automato = open('automato.txt', 'r')
+#         EstadoInicial = automato.readlines()[1].find('n')  # Estado inicial *0
+#         # for  in EstadoInicial:
+
+#         break
+#     break
+
+import pandas as pd
